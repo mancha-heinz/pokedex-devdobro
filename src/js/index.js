c3888b6bc83fa6c30b83c60aeff4e058018bd3ef
mancha-heinz <manchaheinz@gmail.com>
2023-05-14 16:10:15 -0300

    arq .js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 /*
     O que precisamos fazer? - quando clicarmos no botão de troca de tema temos que alterar a cor do tema da página para as cores do tema claro ou do tema escuro
+
     - objetivo 1 - quando clicar no botão de troca de tema, adicionar a classe modo-escuro no body pra que os estilos do modo escuro sejam aplicados e mudar a imagem pra lua
         - passo 1 - pegar no JS o elemento HTML correspondente ao botão de troca de tema
         - passo 2 - dar um jeito de pegar no JS o elemento HTML corresponde ao body
@@ -8,6 +9,25 @@
         - passo 5 - trocar a imagem do botão de alterar tema pra lua
     
     - objetivo 2 - quando clicar no botão de troca de tema, caso o body já tenha a classe modo-escuro, remover a classe pra mudar pro modo claro e mudar a imagem pro sol
-        - passo 6 - remover a classe do modo-escuro do body
-        - passo 7 - trocar a imagem do botão de alterar tema pra sol
+        - passo 6 - verificar se o body já tem a classe modo-escuro
+        - passo 7 - remover a classe do modo-escuro do body
+        - passo 8 - trocar a imagem do botão de alterar tema pra sol
 */
+
+// objetivo 1 - quando clicar no botão de troca de tema, adicionar a classe modo-escuro no body pra que os estilos do modo escuro sejam aplicados e mudar a imagem pra lua
+
+const btnAlterarTema = document.getElementById("btn-alterar-tema");
+// console.log(btnAlterarTema);
+const imgTrocaTema = document.querySelector(".img-botao");
+const body = document.querySelector("body");
+// console.log(body);
+
+btnAlterarTema.addEventListener("click", () => { //'escuta' o elemento
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+    body.classList.toggle("modo-escuro");
+    if (modoEscuroAtivo) {
+        imgTrocaTema.setAttribute("src", "./src/img/sun.png");
+    } else {
+        imgTrocaTema.setAttribute("src", "./src/img/moon.png");
+    }
+});
